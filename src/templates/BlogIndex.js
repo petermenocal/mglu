2d2acedbd5c444bdf9eaffa515febd6dbaddcfb9
Peter Menocal <peter@geekypixel.com>
2019-02-23 20:50:20 -0800

    update cms config

--- src/templates/BlogIndex.js
+++ src/templates/BlogIndex.js
@@ -93,8 +93,7 @@ export const pageQuery = graphql`
     }
 
     posts: allMarkdownRemark(
-      filter: { fields: { contentType: { eq: "posts" } } }
-      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { fields: { contentType: { regex: "/^(posts|hotels)$/" } } }
     ) {
       edges {
         node {
